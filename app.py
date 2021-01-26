88e142760da8dd9c903e2033f28e43293abe51b8
Georgi Tashkov <gtashkov94@gmail.com>
2021-01-26 13:14:31 +0200

    CRUD Fully working

--- app.py
+++ app.py
@@ -52,6 +52,28 @@ def posts():
         all_posts = Blogpost.query.order_by(Blogpost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = Blogpost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+
+    post = Blogpost.query.get_or_404(id)
+
+    if request.method == "POST":
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
     return f'Hello {name} your id is: {id}'
