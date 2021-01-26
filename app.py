893602164b223e56cc324717e39101fa85249557
Georgi Tashkov <gtashkov94@gmail.com>
2021-01-26 16:07:02 +0200

    added some more functionality

--- app.py
+++ app.py
@@ -20,26 +20,14 @@ class Blogpost(db.Model):
     def __repr__(self):
         return f'Blog post {self.id}'
 
-all_posts = [
-    {
-        'title': 'Post1',
-        'content': 'This is the content of Post 1',
-        'author': "Georgi"
-    },
-    {
-        'title': 'Post2',
-        'content': 'This is the content of post 2'
-    }
-]
-
+# welcome screen for the user with two choices
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# this is the uri for our app
+# this is used to create posts in the DB
 @app.route('/posts', methods = ['GET', 'POST'])
 def posts():
-# the logic behind it can`t yet Create or Read, prompots DB
     if request.method == "POST":
         post_title = request.form['title']
         post_content = request.form['content']
@@ -52,6 +40,7 @@ def posts():
         all_posts = Blogpost.query.order_by(Blogpost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
 
+# this is used for deleting the posts in the DB
 @app.route('/posts/delete/<int:id>')
 def delete(id):
     post = Blogpost.query.get_or_404(id)
@@ -59,6 +48,7 @@ def delete(id):
     db.session.commit()
     return redirect('/posts')
 
+# this is used for editing the posts in the DB
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
 
@@ -73,14 +63,10 @@ def edit(id):
     else:
         return render_template('edit.html', post=post)
 
-
-@app.route('/home/users/<string:name>/posts/<int:id>')
-def hello(name, id):
-    return f'Hello {name} your id is: {id}'
-
-@app.route('/onlyget', methods = ['GET', 'POST']) 
-def get_req():
-    return 'You can only get this webpage'
+# This is used when user clicks -> NO
+@app.route('/goodbye')
+def exititing():
+    return render_template('goodbye.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
